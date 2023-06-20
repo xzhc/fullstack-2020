93c4f4862833e1f210a18cd96d64c248fece69f8
xzhc <1838998336@qq.com>
2023-06-20 15:09:49 +0800

    The Phonebook Step2

---
+++ part2/src/exercise/2.7/App.js
@@ -0,0 +1,44 @@
+import { useState, React } from "react"
+const App = () => {
+  const [persons,setPersons] = useState([{name: 'Arto Hellas'}])
+  const [newName, setNewName] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    const person = {name: newName}
+    const nameExits = persons.some((person) => person.name === newName)
+    if ( !nameExits) {
+      setPersons(persons.concat(person))
+      setNewName('')
+      setErrorMessage('')  
+    } else {
+      alert(newName + ' is already added to phonebook')
+    }
+  }
+  const handleNameChange = (event) =>{
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+      <ul>
+        {persons.map((person) =>(
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  ) 
+}
+
+export default App;
