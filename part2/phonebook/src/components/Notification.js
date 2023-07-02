348a5121748c30ad216bcfbd7c0871efadbfa959
xzhc <1838998336@qq.com>
2023-07-02 23:30:57 +0800

    Phonebook

--- part2/phonebook/src/components/Notification.js
+++ part2/phonebook/src/components/Notification.js
@@ -1,10 +1,32 @@
+import React from "react"
+
+const successStyle = {
+    color: 'green',
+    background: 'lightgrey',
+    font_size: 20,
+    border_style: 'solid',
+    border_radius: 5,
+    padding: 10,
+    margin_bottom: 10
+  }
+  
+  const errorStyle = {
+    color: 'red',
+    background: 'lightgrey',
+    font_size: 20,
+    border_style: 'solid',
+    border_radius: 5,
+    padding: 10,
+    margin_bottom: 10
+  }
+
 const Notification = ({message}) => {
     if ( message === null) {
         return null
     }
     if ( message.includes("failed")) {
-        return <div className="error-message">{message}</div>
+        return <div style={errorStyle} >{message}</div>
     }
-    return <div className="success-message">{message}</div>
+    return <div style={successStyle} >{message}</div>
 }
-export default Notification
+export default Notification;
