74463ec8674fafb378e7fd0b0f755422ea329e38
xzhc <1838998336@qq.com>
2023-07-18 14:27:04 +0800

    notebook-backend

--- part3/notebook-backend/index.js
+++ part3/notebook-backend/index.js
@@ -1,81 +1,107 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 
-app.use(express.json())
+const Note = require('./models/note')
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",   
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
-//get root 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World! </h1>')
-})
-//get all note
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-// get a note by id
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-    console.log(note)
-    if ( note) {
-      response.json(note)  
-    } else {
-      response.status(404).end()
-    }
-    
-})
-//delete a note
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
-})
+const requestLogger = (request, response, next) => {
+  console.log('Method', request.method)
+  console.log('Path', request.Path)
+  console.log('Body', request.body)
+  console.log('----')
 
-//generate a Id for new note
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-  return maxId + 1
+  next()
 }
 
-//join a new note
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  if ( !body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if ( error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if ( error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
-  const note = {
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+
+//join a new note
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
-  notes = notes.concat(note)
-  response.json(note)
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+    .catch(error => next(error))
+
 })
 
-const PORT = 3001
+
+
+
+//get root
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World! </h1>')
+})
+//get all note
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+
+
+// get a note by id
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note => {
+    if ( note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
+})
+//delete a note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end()
+  })
+    .catch(error => next(error))
+})
+
+//update a note
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
+    .then(updateNote => {
+      response.json(updateNote)
+    })
+    .catch(error => next(error))
+})
+
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
