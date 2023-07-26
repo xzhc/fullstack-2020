dd36eee7267c6c7b639ee07810253d9e894048de
xzhc <1838998336@qq.com>
2023-07-26 23:10:26 +0800

    test_helper

---
+++ part4/notebook-backend/tests/test_helper.js
@@ -0,0 +1,31 @@
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true
+  }
+]
+
+const noExistingId = async () => {
+  const note = new Note({ content: 'willremovethissoon', date: new Date() })
+  await note.save()
+  await note.deleteOne()
+
+  return note._id.toString()
+}
+
+const notesInDb = async () => {
+  const notes = await Note.find({})
+  return notes.map(note => note.toJSON())
+}
+
+module.exports = {
+  initialNotes, noExistingId, notesInDb
+}
