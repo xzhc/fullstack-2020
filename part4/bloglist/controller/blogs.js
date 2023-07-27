92f236d70fdfb1b7c9190dec2df1660541e7d7c5
xzhc <1838998336@qq.com>
2023-07-27 16:40:00 +0800

    Add blog update and delete logic

--- part4/bloglist/controller/blogs.js
+++ part4/bloglist/controller/blogs.js
@@ -11,14 +11,46 @@ blogsRouter.get('/', (request, response) => {
 
 
 //create a new blog into bloglist
-blogsRouter.post('/',(request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/',async (request, response) => {
+  const body = request.body
 
-  blog.save().then(result => {
-    response.status(201).json(result)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
   })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+
 })
 
+//delete a existed blog
+blogsRouter.delete('/:id', async (request, response) => {
+  const deleteBlog = await Blog.findByIdAndRemove(request.params.id)
+  if ( deleteBlog) {
+    return response.status(204).end()
+  } else {
+    return response.status(404).json({ error: 'Blog not found' })
+  }
+})
+
+//update a existed blog
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes
+  }
+
+  const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true } )
+
+  if ( updateBlog) {
+    response.json(updateBlog)
+  } else {
+    response.status(404).end()
+  }
+})
 
 
 module.exports = blogsRouter
