7ad7af8983e8c95891118829578af9b11a9ea60e
xzhc <1838998336@qq.com>
2023-07-29 23:49:43 +0800

    add user for notes && token authoriaztioon

--- part4/notebook-backend/controller/notes.js
+++ part4/notebook-backend/controller/notes.js
@@ -1,12 +1,24 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 
 //get all notes
 notesRouter.get('/', async(request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
+
+const getFromToken = request => {
+  const authorization = request.get('authorization')
+
+  if ( authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+
+  return null
+}
 
 //get a note by its id
 notesRouter.get('/:id', async(request, response) => {
@@ -21,13 +33,23 @@ notesRouter.get('/:id', async(request, response) => {
 //create a new note
 notesRouter.post('/',async (request, response) => {
   const body = request.body
+  const token = getFromToken(request)
+  const decodeToken = jwt.verify(token, process.env.SECRET)
+
+  if ( !decodeToken.id) {
+    return response.status(401).json( { error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodeToken.id)
 
   const note = new Note({
     content:body.content,
     date: new Date(),
-    important: body.important || false
+    important: body.important || false,
+    user: user._id
   })
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
